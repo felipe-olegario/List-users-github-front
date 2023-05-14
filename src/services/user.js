943800220754edd42ff03bd94bc9cb2dea34fe28
felipe.olegario <felipe.olegario@bandtec.com.br>
2023-05-13 21:08:30 -0300

    fix: adjust host

--- src/services/user.js
+++ src/services/user.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     async listUsers(since) {
-        return axios.get(`http://3.93.220.89:3001/user/list/${since}`);
+        return axios.get(`http://3.82.122.231:3001/user/list/${since}`);
     },
     async getUser(login) {
-        return axios.get(`http://3.93.220.89:3001/user/${login}`);
+        return axios.get(`http://3.82.122.231:3001/user/${login}`);
     },
     async getRepositories(login) {
-        return axios.get(`http://3.93.220.89:3001/user/repos/${login}`);
+        return axios.get(`http://3.82.122.231:3001/user/repos/${login}`);
     }
 };
